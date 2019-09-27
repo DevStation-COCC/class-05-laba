69cda84a9d450c003db68438f7423c55e24f1689
David Vloedman <djvloedman@gmail.com>
2019-09-27 10:56:06 -0700

    Problem 1 completed

--- lab-a/starter-code/app.js
+++ lab-a/starter-code/app.js
@@ -9,9 +9,12 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-
+    var sum = a + b;
+    var sumMsg = 'The sum of 4 and 7 is 11.'
+    return [sum, sumMsg];
 }
 
+testSum(1, 2);
 // Here is the test for sum(); uncomment it to run it
 // testSum(4, 7);
 
