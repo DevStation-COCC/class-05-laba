846068f6b184b599a5d55031adc8778e4c60ced5
Trevor Thompson <Trevor.Ray.Thompson@gmail.com>
2019-09-27 12:31:34 -0700

    problem5 completed

--- lab-a/starter-code/app.js
+++ lab-a/starter-code/app.js
@@ -110,11 +110,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
+    var productArray1 = multiply(multArr[0], multArr[1])[0];
+    var productArray2 = multiply(productArray1, multArr[2])[0];
+
+    var string = 'The numbers 2,3,4 have a product of 24.'
+    return [productArray2, string];
 
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
