9b5ee37b4a551d6eadd78020c63c0376b83b8daf
david-vloedman <55366082+david-vloedman@users.noreply.github.com>
2019-10-03 15:00:18 -0700

    Update app.js
    Removed hard coded forgotten thing

--- lab-a/starter-code/app.js
+++ lab-a/starter-code/app.js
@@ -10,7 +10,7 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
     var sum = a + b;
-    var sumMsg = 'The sum of ' + a + ' and ' +  7 + ' is ' + sum + '.'; 
+    var sumMsg = 'The sum of ' + a + ' and ' +  b + ' is ' + sum + '.'; 
     return [sum, sumMsg];
 }
 
